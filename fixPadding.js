eea039318e896a074eefd039f1fbc8f47c08eefb
Nelson Gomes Neto <ngomesneto@outlook.com>
2020-09-16 14:25:06 -0300

    Fix WhatsApp Web's padding

---
+++ fixPadding.js
@@ -0,0 +1,5 @@
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML = ".h70RQ { width: 100% !important; height: 100% !important; top: 0 !important; }";
+document.body.appendChild(css);
+console.log("Successfully removed padding");
